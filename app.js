ff7cbe6db92078c557431915164b5f77d6967733
TheoC26 <91738250+TheoC26@users.noreply.github.com>
2024-01-30 13:49:18 -0500

    bug fixes

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const matterContainer = document.getElementById("matter-container");
 const nextFruitRef = document.querySelector(".next-fruit");
 
 const bounciness = 0.5;
+const friction = 0.6;
 
 const schools = [
+  {
+    name: "nyc",
+    size: 110,
+    scaler: 0.0034,
+  },
   {
     name: "Dalton",
     size: 100,
@@ -58,12 +64,26 @@ const schools = [
 
 let objects = [];
 let score = 0;
-let daltons = 0;
-let nextSchool = 9;
+let nycs = 0;
+let nextSchool = 10;
 let canClick = true;
+let gameOver = false;
+
+// if there is no high score in local storage, then set the high score to 0
+if (!localStorage.getItem("highScore")) {
+  localStorage.setItem("highScore", 0);
+} else {
+    document.querySelector(".high-score span").innerText = localStorage.getItem("highScore");
+}
+if (!localStorage.getItem("nycs")) {
+  localStorage.setItem("nycs", 0);
+} else {
+    console.log(localStorage.getItem("nycs"))
+    document.querySelector(".nycs span").innerText = localStorage.getItem("nycs");
+}
 
 function chooseNextSchool() {
-  nextSchool = 9 - Math.floor(Math.random() * 4);
+  nextSchool = 10 - Math.floor(Math.random() * 4);
 
   nextFruitRef.style.backgroundImage = `url(Images/${schools[nextSchool].name}.png)`;
   // change the width and height
@@ -81,7 +101,11 @@ function deleteSchool() {
   //wait for 1 second
   setTimeout(() => {
     canClick = true;
-    chooseNextSchool();
+    checkGameOver();
+    if (!gameOver) {
+      chooseNextSchool();
+    } else {
+    }
   }, 1250);
 }
 
@@ -95,6 +119,45 @@ function getSchoolIndex(label) {
   return -1;
 }
 
+// check game over by looping through all of the objects and if any of them are above the screen, then the game is over
+function checkGameOver() {
+  for (let i = 0; i < objects.length; i++) {
+    if (
+      objects[i].position.y - objects[i].circleRadius <
+      215 * (matterContainer.clientWidth / 831)
+    ) {
+      gameOver = true;
+      gameOverState();
+    }
+  }
+}
+
+function gameOverState() {
+  console.log("Game Over");
+  // loop through all of the objects and remove them but wait one second before removing each one
+  for (let i = objects.length-1; i >=0; i--) {
+    setTimeout(() => {
+     score += 11 - getSchoolIndex(objects[i].label);
+     Matter.World.remove(engine.world, objects[i]);
+     document.querySelector(".score span").innerText = score;
+
+     if (i === 0) {
+        // if the score is greater than the high score, then set the high score to the score
+        if (score > localStorage.getItem("highScore")) {
+          localStorage.setItem("highScore", score);
+          document.querySelector(".high-score span").innerText = score;
+        }
+    //    objects = [];
+    //    score = 0;
+    //    document.querySelector(".score span").innerText = score;
+    //    chooseNextSchool();
+    //    gameOver = false;
+     }
+    }, i * 500);
+  }
+}
+
+
 let engine = new Matter.Engine.create();
 let render = Matter.Render.create({
   element: matterContainer,
@@ -103,12 +166,22 @@ let render = Matter.Render.create({
     width: matterContainer.clientWidth,
     height: matterContainer.clientHeight,
     wireframes: false,
-    background: "white",
+    background: "transparent",
   },
 });
 
 // change gravity to double
 engine.world.gravity.y = 2;
+
+console.log(matterContainer.clientWidth);
+
+let ceiling = Matter.Bodies.rectangle(
+  matterContainer.clientWidth / 2,
+  -20,
+  27184,
+  20,
+  { isStatic: true, label: "ceiling", render: { fillStyle: "transparent" } }
+);
 
 // make the color of the ground transparent
 let ground = Matter.Bodies.rectangle(
@@ -133,7 +206,6 @@ let rightWall = Matter.Bodies.rectangle(
   { isStatic: true, label: "rightWall", render: { fillStyle: "transparent" } }
 );
 
-
 let mouse = Matter.Mouse.create(render.canvas);
 let mouseConstraint = Matter.MouseConstraint.create(engine, {
   mouse: mouse,
@@ -144,6 +216,7 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
 render.mouse = mouse;
 
 Matter.World.add(engine.world, [
+  ceiling,
   ground,
   leftWall,
   rightWall,
@@ -152,16 +225,17 @@ Matter.World.add(engine.world, [
 ]);
 
 Matter.Events.on(mouseConstraint, "mousedown", function (event) {
-  if (!canClick) {
+  if (!canClick || gameOver) {
     return;
   }
   const school = schools[nextSchool];
   let body = Matter.Bodies.circle(
-    event.mouse.position.x*2,
+    event.mouse.position.x * 2,
     40 * (matterContainer.clientWidth / 416),
     school.size * (matterContainer.clientWidth / 416),
     {
       restitution: bounciness,
+      friction: friction,
       label: school.name,
       render: {
         // fillStyle: colors[Math.floor(Math.random() * colors.length)],
@@ -186,11 +260,22 @@ Matter.Events.on(engine, "collisionStart", function (event) {
     const pair = pairs[i];
 
     if (pair.bodyA.label === pair.bodyB.label) {
-        // play sound
-        // let audio = new Audio("Plop.mov");
-        // audio.play();
-        score += 10 - getSchoolIndex(pair.bodyA.label);
-        document.querySelector(".score span").innerText = score;
+      // play sound
+      // let audio = new Audio("Plop.mov");
+      // audio.play();
+      score += 11 - getSchoolIndex(pair.bodyA.label);
+      document.querySelector(".score span").innerText = score;
+
+      // if the pair.bodyA.label is nyc, then add 1 to nycs and also wait 1 second and then delete the nyc
+        if (pair.bodyA.label === "nyc") {
+            nycs++;
+            localStorage.setItem("nycs", nycs);
+            document.querySelector(".nycs span").innerText = nycs;
+            setTimeout(() => {
+                Matter.World.remove(engine.world, pair.bodyA);
+            }, 1000);
+        }
+
       const newSchoolIndex = getSchoolIndex(pair.bodyA.label) - 1;
       const newSchoolRef = schools[newSchoolIndex];
       const newSchool = Matter.Bodies.circle(
@@ -200,6 +285,7 @@ Matter.Events.on(engine, "collisionStart", function (event) {
         newSchoolRef.size * (matterContainer.clientWidth / 416),
         {
           restitution: bounciness,
+          friction: friction,
           label: newSchoolRef.name,
           render: {
             sprite: {
@@ -221,6 +307,11 @@ Matter.Events.on(engine, "collisionStart", function (event) {
       Matter.World.remove(engine.world, pair.bodyB);
       Matter.World.add(engine.world, newSchool);
       objects.push(newSchool);
+      // delete the old schools
+      objects = objects.filter(
+        (object) => object !== pair.bodyA && object !== pair.bodyB
+      );
+      //   console.log(objects);
     }
   }
 });
